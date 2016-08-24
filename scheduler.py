e1b407af5404baceb6157d9d15e3cc3c9d663aac
takechanman1228 <sigur.ku@gmail.com>
2016-08-24 15:30:00 +0900

    scheduler

--- scheduler.py
+++ scheduler.py
@@ -1,10 +1,45 @@
 import schedule
 import time
 import linebot
+import requests
+import json
+import re
+from datetime import datetime
+
+LINEBOT_API_EVENT ='https://trialbot-api.line.me/v1/events'
+LINEBOT_API_IMAHE_VIDEO = 'https://trialbot-api.line.me/v1/bot/message/'
+LINE_HEADERS = {
+    'Content-type': 'application/json; charset=UTF-8',
+    'X-Line-ChannelID':'1469904360', # Channel ID
+    'X-Line-ChannelSecret':'cadb3352a866e7811c1a5d8d655e3f91', # Channel secre
+    'X-Line-Trusted-User-With-ACL':'u48d6abf59024909b4a3eae290539188e' # MID (of Channel)
+}
+
+
+def post_text( to, text ):
+    content = {
+        'contentType':1,
+        'toType':1,
+        'text':text,
+    }
+    post_event(to, content)
+
+def post_event( to, content):
+    msg = {
+        'to': [to],
+        # 'to': u7d8c4b981b6d3b93ff38bb89f6d1c5ae,
+        'toChannel': 1383378250, # Fixed  value
+        'eventType': "138311608800106203", # Fixed value
+        'content': content
+    }
+    print(content)
+    r = requests.post(LINEBOT_API_EVENT, headers=LINE_HEADERS, data=json.dumps(msg))
 
 def job():
+
     print("I'm working...")
-    hello()
+
+    # hello()
     post_text('u206d25c2ea6bd87c17655609a1c37cb8',"時報だよ")
 
 schedule.every(1).minutes.do(job)
